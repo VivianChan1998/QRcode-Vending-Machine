0bbd832eee0ea6191a6c8bf850410bde1117b458
Vivian <b05901179@ntu.edu.tw>
2019-06-24 22:47:16 +0800

    heroku debugging

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render(__dirname + '/pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 //server.listen(port, '0.0.0.0');
