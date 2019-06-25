c66827fd627e6835c9d5813348b273f60d0aa69f
Vivian <b05901179@ntu.edu.tw>
2019-06-25 18:59:25 +0800

    keep on debugging

--- src/components/Good.js
+++ src/components/Good.js
@@ -23,7 +23,7 @@ class Good extends Component {
     handleOrderCLick = () => {
         var pw = uuid()
         console.log(require("ip").address())
-        fetch('http://localhost:3001/api/choose?id='+this.state.ID+'&pw='+pw)
+        fetch('/api/choose?id='+this.state.ID+'&pw='+pw)
             .then(res => res.json())
             .then(res => {
                 console.log(res)
